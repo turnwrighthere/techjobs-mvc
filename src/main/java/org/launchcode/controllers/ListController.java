5a99d900bc96989287485a33aac49a48846baa3f
turnwrighthere <j.a.wright2010@gmail.com>
2018-06-04 20:14:52 -0500

    Completed the assignment!

--- src/main/java/org/launchcode/controllers/ListController.java
+++ src/main/java/org/launchcode/controllers/ListController.java
@@ -38,9 +38,8 @@ public class ListController {
     public String listColumnValues(Model model, @RequestParam String column) {
 
         if (column.equals("all")) {
-            ArrayList<HashMap<String, String>> jobs = JobData.findAll();
-            model.addAttribute("title", "All Jobs");
-            model.addAttribute("jobs", jobs);
+            ArrayList<HashMap<String, String>> listAllResults = JobData.findAll();
+            model.addAttribute("listAllResults", listAllResults);
             return "list-jobs";
         } else {
             ArrayList<String> items = JobData.findAll(column);
@@ -62,4 +61,5 @@ public class ListController {
 
         return "list-jobs";
     }
+
 }
--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,10 +4,13 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static org.launchcode.models.JobData.findByColumnAndValue;
 
 /**
  * Created by LaunchCode
@@ -23,5 +26,23 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+
+    @RequestMapping(value = "results")
+    public String search(Model model, String searchType, String searchTerm) {
+        model.addAttribute("columns", ListController.columnChoices);
+
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> searchResults = JobData.findByValue(searchTerm);
+            model.addAttribute("searchResults", searchResults);
+            return "search";
+        } else {
+            ArrayList<HashMap<String, String>> searchResults = findByColumnAndValue(searchType, searchTerm);
+
+            //This line allows the html template to use the variable
+            model.addAttribute("searchResults", searchResults);
+
+            return "search";
+        }
+    }
 
 }
